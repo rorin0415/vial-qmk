e163e359b00fd0d071c83838993195db47896af9
ryohei.shiraishi <ryohei.shiraishi@proassist.co.jp>
2025-12-01 18:03:24 +0900

    対応

--- keyboards/rorin/simron_v1/keymaps/vial/keymap.c
+++ keyboards/rorin/simron_v1/keymaps/vial/keymap.c
@@ -32,6 +32,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO    , KC_F1    , KC_F2    , KC_F3    , KC_F12   ,        KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO       , 
     KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,        KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO
   ),
+
+  [4] = LAYOUT(
+    KC_Q,         KC_W,       KC_E,       KC_R,       KC_T,           KC_Y,       KC_U,         KC_I,         KC_O,       KC_P,
+    LCTL_T(KC_A), KC_S,       KC_D,       KC_F,       KC_G,           KC_H,       KC_J,         KC_K,         KC_L,       KC_ENTER,
+    LSFT_T(KC_Z), KC_X,       KC_C,       KC_V,       KC_B,           KC_N,       KC_M,         KC_COMMA,     KC_DOT,     LT(2, KC_SLASH),
+    KC_LCTL,     KC_LSFT,  KC_LGUI,    KC_LALT,    LT(1, KC_SPACE),  KC_BSPC,  KC_TAB,       KC_NO,     KC_NO,   MO(3)
+  ),
+
+  [5] = LAYOUT(
+    KC_ESC,      KC_7,       KC_8,       KC_9,       KC_NO,          KC_NO,      KC_NO,        KC_GRAVE,     KC_MINUS,   KC_EQUAL,
+    KC_0,         KC_4,       KC_5,       KC_6,       KC_NO,          KC_NO,      KC_NO,        KC_SCLN,    KC_QUOTE,   KC_BSLS,
+    KC_LSFT,    KC_1,       KC_2,       KC_3,       KC_NO,          KC_NO,      KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_RSFT,
+    KC_TRNS,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_DELETE,  KC_TRNS,      KC_TRNS,      KC_TRNS,    KC_TRNS
+  ),
+
+  [6] = LAYOUT(
+    LSFT(KC_1),   LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),     LSFT(KC_6), LSFT(KC_7),   LSFT(KC_8),   LSFT(KC_9), LSFT(KC_0),
+    KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,        KC_UP,        KC_NO,      KC_NO,
+    KC_LSFT,    KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_LEFT,      KC_DOWN,      KC_RIGHT,   KC_NO,
+    KC_TRNS,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_LSFT,      KC_TRNS,    KC_TRNS,      KC_TRNS,      KC_TRNS,    KC_TRNS
+  ),
+
+  [7] = LAYOUT(
+    KC_NO,        KC_F7,      KC_F8,      KC_F9,      KC_F10,         KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_PSCR,
+    LCTL(KC_A),   KC_F4,      KC_F5,      KC_F6,      KC_F11,         KC_NO,      KC_NO,        KC_PGUP,      KC_NO,      KC_NO,
+    LSFT(KC_Z),   KC_F1,      KC_F2,      KC_F3,      KC_F12,         KC_NO,      KC_HOME,      KC_PGDN,    KC_END,     KC_RSFT,
+    KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,        KC_NO,        KC_NO,      KC_NO
+  ),
+
 };
 // clang-format on
 

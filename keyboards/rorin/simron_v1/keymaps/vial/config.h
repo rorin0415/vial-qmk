4b12c9db04604d5e3ea7cea30e467f59fdec5594
ryohei.shiraishi <ryohei.shiraishi@proassist.co.jp>
2025-12-02 12:19:21 +0900

    更新

--- keyboards/rorin/simron_v1/keymaps/vial/config.h
+++ keyboards/rorin/simron_v1/keymaps/vial/config.h
@@ -1,4 +1,5 @@
 #pragma once
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define VIAL_KEYBOARD_UID {0x73, 0x69, 0x6D, 0x72, 0x6F, 0x6E, 0x30, 0x31}
 #define VIAL_UNLOCK_COMBO_ROWS {0, 1}
 #define VIAL_UNLOCK_COMBO_COLS {0, 1}
